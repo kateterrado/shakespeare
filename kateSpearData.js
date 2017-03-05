ba26c79f8272f00602d61a80b6dfb75541e368c2
Francisco Dias <fdias@hubspot.com>
2017-03-05 08:52:33 -0800

    put some code up to explain and talk about how to refactor

---
+++ kateSpearData.js
@@ -0,0 +1,17 @@
+const trumpWords = [
+  {
+    word: "bigly",
+    context: "We are going to win bigly!",
+    tweet: "bigly is infact a word."
+  },
+  {
+    word: "Tremendous",
+    context: "I have tremendous hands.",
+    tweet: "TREMENDOUS"
+  },
+  {
+    word: "SAD",
+    context: "Usually people feel emtions but today the state of the union actually feels sad.",
+    tweet: "SAD"
+  }
+];
